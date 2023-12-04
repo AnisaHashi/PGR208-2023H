897c3e26dcaba6de1196e65d5cd9c01e273a73a6
Anisa Hashi <aniso1020@gmail.com>
2023-12-04 22:17:44 +0100

    Laget styling av innholdet

--- app/src/main/java/com/example/pgr208_2023h/MainActivity.kt
+++ app/src/main/java/com/example/pgr208_2023h/MainActivity.kt
@@ -3,19 +3,36 @@ package com.example.pgr208_2023h
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,10 +41,16 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.pgr208_2023h.ui.theme.PGR2082023HTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,32 +58,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PGR2082023HTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
 
                 ) {
-
-                    // Header
-                    MediumTopAppBarExample()
-                    // List
+                    Content()
                 }
             }
         }
     }
 }
 
-// List
-// loop Card
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MediumTopAppBarExample() {
+fun Content() {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -95,9 +110,47 @@ fun MediumTopAppBarExample() {
         ScrollContent(innerPadding)
     }
 }
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val category: String,
+    val price: Double,
+    val image: String
+)
+
+val product1 =
+    Product(
+        id = 1,
+        title = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        category = "men's clothing",
+        price = 109.95,
+        image = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+    )
+
+val product2 =
+    Product(
+        id = 2,
+        title = "Mens Casual Premium Slim Fit T-Shirts",
+        category = "men's clothing",
+        price = 22.3,
+        image = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+    )
+
+val product3 =
+    Product(
+        id = 3,
+        title = "Mens Cotton Jacket",
+        category = "men's clothing",
+        price = 55.99,
+        image = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+    )
+
+val products = listOf(product1, product2, product3)
+
 @Composable
 fun ScrollContent(innerPadding: PaddingValues) {
-    val range = 1..100
+    val range = 1..10
 
     LazyColumn(
         modifier = Modifier
@@ -105,8 +158,61 @@ fun ScrollContent(innerPadding: PaddingValues) {
         contentPadding = innerPadding,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(range.count()) { index ->
-            Text(text = "- List item number ${index + 1}")
+        items(products) { product ->
+            ProductCard(product = product)
+        }
+    }
+}
+
+@Composable
+fun ProductCard(product: Product) {
+    Card(
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(16.dp)
+        ) {
+            // Image on the left
+            Image(
+                painter = rememberImagePainter(
+                    data = product.image, // Replace with your image URL
+                    builder = {
+                        crossfade(true)
+                    }
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.White)
+            )
+
+            // Spacer to separate image and text
+             Spacer(modifier = Modifier.width(16.dp))
+
+            // Title and text on the right
+            Column {
+                Text(
+                    text = product.title,
+                    style = MaterialTheme.typography.titleMedium,
+
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = product.category,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = "$${product.price}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
